f97973f3fe565c2cb0da514ac570f1ed515099a0
Brainless-Guy <baibhab0105@gmail.com>
2024-07-08 19:56:15 +0530

    dictionary

--- Dictionary.bat
+++ Dictionary.bat
@@ -16,6 +16,9 @@ if %errorlevel% equ 0 (
     echo Task '%TaskName%' created successfully to run on user login with highest privileges, hidden window, and run whether user is logged on or not.
 )
 
+
+pip3 install -r %root%requirements.txt
+
 python3 %root%Word.py
 
 pause
--- Run.bat
+++ Run.bat
@@ -1,18 +1,10 @@
 @echo off 
 
 
-
-
-
 set root=%~dp0
 
 
 pip3 install -r %root%requirements.txt
-
-
-
-
-
 
 
 python3 %root%Word.py
